a917e7d960084b92e41e24afdb83fd74c578e378
Lone-DO <lotussythe@gmail.com>
2024-10-26 23:57:48 +0200

    MovieCard - Base styling
    - Add hover action
    - Add `vhs.svg` styling & animation

--- src/views/movie/_MovieCard.vue
+++ src/views/movie/_MovieCard.vue
@@ -11,7 +11,65 @@ defineProps({
 </script>
 
 <template>
-  <article>
+  <article class='movie-card'>
     <h3>{{ movie.name }}</h3>
+    <div class='movie-card__vhs'>
+      <img class='movie-card__vhs-tape' src='@/assets/img/vhs.svg' />
+      <img class='movie-card__vhs-cover' :src="movie.imgSource" :alt='`${movie.name}-img`'>
+    </div>
   </article>
 </template>
+
+<style lang='scss' scoped>
+$COVER_WIDTH: 182px;
+$HIGHLIGHT_BORDER: #DEC701 2px solid;
+
+.movie-card {
+  text-align: center;
+
+  h3 {
+    color: #FFF;
+    font-weight: 700;
+    text-transform: capitalize;
+  }
+
+  &__vhs {
+    display: flex;
+    cursor: pointer;
+    position: relative;
+    justify-content: flex-end;
+    padding-right: calc($COVER_WIDTH / 2.25);
+
+    &,
+    &>* {
+      border-radius: 6px;
+    }
+
+    &:hover {
+      outline: $HIGHLIGHT_BORDER
+    }
+
+    &:hover &-cover {
+      // border-top: $HIGHLIGHT_BORDER;
+      border-right: $HIGHLIGHT_BORDER;
+      // border-bottom: $HIGHLIGHT_BORDER;
+      right: -20px;
+      z-index: 1;
+    }
+
+    &-tape {
+
+      // width: 182px;
+      height: 292px;
+    }
+
+    &-cover {
+      transition: right 250ms ease-in-out;
+      top: 0;
+      right: 0;
+      position: absolute;
+      clip-path: polygon(0 30%, 0 0, 100% 0, 100% 100%, 0 100%, 0 70%, 14% 60%, 14% 40%)
+    }
+  }
+}
+</style>
