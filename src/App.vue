e841a474b32a54ed2bd21f88adccc67943240dbd
Lone-DO <lotussythe@gmail.com>
2024-10-27 04:08:51 +0100

    App - Update Base Styling

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router';
+<script setup lang='ts'>
+import { RouterLink, RouterView, useRoute } from 'vue-router';
+
+const route = useRoute()
+
+function isActive(type: string) {
+  if (!type) return false;
+  return type === 'home' ? route.fullPath === '/' : route.fullPath.includes(type)
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/icons/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/bangboo">Bangboo</RouterLink>
-        <RouterLink to="/character">Characters</RouterLink>
-        <RouterLink to="/movie">Movies</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/" :data-active='isActive("home")'>Home</RouterLink>
+      <RouterLink to="/about" :data-active='isActive("about")'>About</RouterLink>
+      <RouterLink to="/bangboo" :data-active='isActive("bangboo")'>Bangboo</RouterLink>
+      <RouterLink to="/character" :data-active='isActive("character")'>Characters</RouterLink>
+      <RouterLink to="/movie" :data-active='isActive("movie")'>Movies</RouterLink>
+    </nav>
   </header>
   <main>
     <RouterView />
@@ -25,7 +28,29 @@ import { RouterLink, RouterView } from 'vue-router';
 </template>
 
 <style lang='scss' scoped>
+header {
+  width: 100%;
+
+  nav {
+    gap: 16px;
+    display: flex;
+
+    * {
+      color: white;
+      padding: 4px;
+      border-radius: 8px;
+      font-weight: $FONT_WEIGHT;
+
+      &[data-active="true"] {
+        outline: $HIGHLIGHT_BORDER;
+      }
+    }
+  }
+}
+
+header,
 main {
-  padding: 16px;
+  padding: 16px 32px;
+  background-color: black;
 }
 </style>
