7daefb13c9ff71013ff8b4b9e45321702733cb4d
lone-DO <citizin@outlook.com>
2025-07-06 17:21:48 +0200

    Vite build `outDir - docs` for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig(({ mode }): UserConfig => {
       },
     ],
     build: {
+      outDir: 'docs',
       emptyOutDir: true,
       minify: false,
       sourcemap: true,
